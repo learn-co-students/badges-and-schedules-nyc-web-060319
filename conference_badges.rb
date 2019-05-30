fef36c3d234424f625eb3911b4ab27400871900f
Cindy Chen <chen_cx0128@hotmail.com>
2019-05-30 19:52:27 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
 # Write your code here.
+
+list = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def batch_badge_creator(list)
+  i = 0
+  while i < list.size
+    puts "Hello, my name is #{list[i]}."
+    i++
+end
+
+def assign_rooms(list)
+  i = 0
+  while i < list.size
+  puts "Hello, #{list[i]}!You'll be assigned to room #{i+1}!"
+  i++
+end
+
