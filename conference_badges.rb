4500a8ac9b5ec6ce040a0a22d46f1fc6af3ff70c
Ingrid Fuentes <ingridfmelgar@gmail.com>
2019-05-31 05:19:42 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,7 @@
-# Write your code here.
+badge_maker("name")   # Write your code here.
+   return "Hello, my name is #{name}."
+
+
+
+assign_rooms("attendees")
+   attendees.each_with_index {|attendees, index| puts "Hello,#{attendees}! You'll be assigned to room #{index} !"
